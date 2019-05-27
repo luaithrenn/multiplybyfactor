8363bd997d08f935984fd730ec2686d8bab91da0
luaithrenn <luaidemp@ie.ibm.com>
2019-05-27 20:17:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+#!/us r/bin/env python
+
 from setuptools import setup, find_packages
 
 setup(
   name='multiplybyfactor',
-  version='1.0',
+  version='0.0.1',
   packages=find_packages(),
+  install_requires==['dill'
+  ],                   
 )
